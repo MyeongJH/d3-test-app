c598d6c8d049f3ca6f8e4b7e216dd6e1c024c0b4
Myeong <myeong1001@gmail.com>
2019-08-09 17:48:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+function FnArr({no, ko}) {
+  console.log(ko,no);
+  return <h3>no {no} ko {ko}</h3>
+}
+
+const arr = [
+  {no : 1, ko : 'ㄱ', en : 'a'}
+  ,{no : 2, ko : 'ㄴ', en : 'b'}
+  ,{no : 3, ko : 'ㄷ', en : 'c'}
+  ,{no : 4, ko : 'ㄹ', en : 'd'}
+  ,{no : 5, ko : 'ㅁ', en : 'e'}
+];
+
 function App() {
   return (
-    <div className="App">
-      test
+    <div>
+      {arr.map( i => (
+        <FnArr no={i.no} ko={i.ko} />
+      ))}
     </div>
   );
 }
