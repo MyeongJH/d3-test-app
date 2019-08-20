160ce9aa5256237f9c35a7d228b72ca2acad7b67
Myeong <myeong1001@gmail.com>
2019-08-20 09:47:58 +0900

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ZoomCirclePacking from './ZoomCirclePacking'
 import Matrix from './Matrix';
 import HierarchiBar from './HierarchiBar';
 
+const baseUrl = '/d3-test-app/';
 const arr = [
   {no : 1, ko : 'ㄱ', en : 'a'}
   ,{no : 2, ko : 'ㄴ', en : 'b'}
@@ -25,68 +26,36 @@ function FnArr({no, ko}) {
 function FnTo() {
   return (
     <div>
-      {arr.map(i => (<FnArr no={i.no} ko={i.ko} />))}
+      {arr.map((d,i) => (<FnArr no={d.no} ko={d.ko} key={i} />))}
       <UiTest />
     </div>
   )
-}
-
-function ToBarChart() {
-  return <BarChart />
-}
-
-function ToBubbleChart() {
-  return <BubbleChart />
-}
-
-function ToTreeMap() {
-  return <TreeMap />
-}
-
-function ToTidyTree() {
-  return <TidyTree />
-}
-
-function ToCirclePacking() {
-  return <CirclePacking />
-}
-
-function ToZoomCirclePacking() {
-  return <ZoomCirclePacking />
-}
-
-function ToMatrix() {
-  return <Matrix />
-}
-
-function ToHierarchiBar() {
-  return <HierarchiBar />
 }
 
 function App() {
   return (
     <Router>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/BarChart">BarChart</Link></li>
-        <li><Link to="/BubbleChart">BubbleChart</Link></li>
-        <li><Link to="/TreeMap">TreeMap</Link></li>
-        <li><Link to="/TidyTree">TidyTree</Link></li>
-        <li><Link to="/CirclePacking">CirclePacking</Link></li>
-        <li><Link to="/ZoomCirclePacking">ZoomCirclePacking</Link></li>
-        <li><Link to="/Matrix">Matrix</Link></li>
-        <li><Link to="/hierarchiBar">hierarchiBar</Link></li>
+        <li><Link to={baseUrl}>Home</Link></li>
+        <li><Link to={baseUrl+"BarChart"}>BarChart</Link></li>
+        <li><Link to={baseUrl+"BubbleChart"}>BubbleChart</Link></li>
+        <li><Link to={baseUrl+"TreeMap"}>TreeMap</Link></li>
+        <li><Link to={baseUrl+"TidyTree"}>TidyTree</Link></li>
+        <li><Link to={baseUrl+"CirclePacking"}>CirclePacking</Link></li>
+        <li><Link to={baseUrl+"ZoomCirclePacking"}>ZoomCirclePacking</Link></li>
+        <li><Link to={baseUrl+"Matrix"}>Matrix</Link></li>
+        <li><Link to={baseUrl+"HierarchiBar"}>HierarchiBar</Link></li>
       </ul>
 
-      <Route exact path="/" component={FnTo} />
-      <Route path="/BarChart" component={ToBarChart} />
-      <Route path="/BubbleChart" component={ToBubbleChart} />
-      <Route path="/TreeMap" component={ToTreeMap} />
-      <Route path="/TidyTree" component={ToTidyTree} />
-      <Route path="/CirclePacking" component={ToCirclePacking} />
-      <Route path="/ZoomCirclePacking" component={ToZoomCirclePacking} />
-      <Route path="/Matrix" component={ToMatrix} />
-      <Route path="/HierarchiBar" component={ToHierarchiBar} />
+      <Route exact path={baseUrl} render={()=><FnTo />} />
+      <Route path={baseUrl+"BarChart"} render={()=><BarChart />} />
+      <Route path={baseUrl+"BubbleChart"} render={()=><BubbleChart />} />
+      <Route path={baseUrl+"TreeMap"} render={()=><TreeMap />} />
+      <Route path={baseUrl+"TidyTree"} render={()=><TidyTree />} />
+      <Route path={baseUrl+"CirclePacking"} render={()=><CirclePacking />} />
+      <Route path={baseUrl+"ZoomCirclePacking"} render={()=><ZoomCirclePacking />} />
+      <Route path={baseUrl+"Matrix"} render={()=><Matrix />} />
+      <Route path={baseUrl+"HierarchiBar"} render={()=><HierarchiBar />} />
     </Router>
   );
 }
